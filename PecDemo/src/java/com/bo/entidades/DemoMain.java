b4a2dac8c366023b0c1c704991ebe55eea2d924d
aCallejas <adalidvega@gmail.com>
2016-05-15 16:52:32 -0400

    adicionando DemoMain

---
+++ PecDemo/src/java/com/bo/entidades/DemoMain.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bo.entidades;
+
+/**
+ *
+ * @author aCallejas
+ */
+public class DemoMain {
+
+    private String persona;
+    private String cuenta;
+    private String relacion;
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getRelacion() {
+        return relacion;
+    }
+
+    public void setRelacion(String relacion) {
+        this.relacion = relacion;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MNDO");
+    }
+
+}
