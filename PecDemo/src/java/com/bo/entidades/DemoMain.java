08127e19840fc6941abed2d94735eefba2b78d0c
aCallejas <adalidvega@gmail.com>
2016-05-15 19:43:01 -0400

    comit desde usuario 2

--- PecDemo/src/java/com/bo/entidades/DemoMain.java
+++ PecDemo/src/java/com/bo/entidades/DemoMain.java
@@ -40,16 +40,7 @@ public class DemoMain {
     }
 
     public static void main(String[] args) {
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
-        System.out.println("HOLA MNDO");
+        
         String persona2;
         String persona3;
         String persona4;
